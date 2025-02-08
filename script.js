b67e67ed7ef9cb911f4ba923f75c6e8c1ffeda78
colinslda <colin.soldati@icloud.com>
2025-02-08 13:11:46 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Gestion des onglets
-const tabButtons = document.querySelectorAll('.tab-button');
+const navButtons = document.querySelectorAll('.nav-button');
 const tabContents = document.querySelectorAll('.tab-content');
 
-tabButtons.forEach(button => {
+navButtons.forEach(button => {
     button.addEventListener('click', () => {
         const tab = button.getAttribute('data-tab');
-        tabButtons.forEach(btn => btn.classList.remove('active'));
+        navButtons.forEach(btn => btn.classList.remove('active'));
         tabContents.forEach(content => content.classList.remove('active'));
         button.classList.add('active');
         document.getElementById(tab).classList.add('active');
