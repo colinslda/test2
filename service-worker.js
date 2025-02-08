027c4cbac0469ac5cc7c8595d4f8fee603fe1cdc
colinslda <colin.soldati@icloud.com>
2025-02-08 13:06:13 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,8 +6,8 @@ self.addEventListener('install', (event) => {
                 '/index.html',
                 '/styles.css',
                 '/script.js',
-                '/icon-192x192.png',
-                '/icon-512x512.png'
+                '/icons/icon-192x192.png',
+                '/icons/icon-512x512.png'
             ]);
         })
     );
